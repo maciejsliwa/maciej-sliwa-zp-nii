d94bc31223ad93bcaac10fd0da6d032528d1d364
maciejsliwa <117169530+maciejsliwa@users.noreply.github.com>
2022-12-04 22:50:33 +0100

    Init code.

---
+++ src/app/main.py
@@ -0,0 +1,22 @@
+import sympy
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/prime/{number}")
+async def is_prime_number(number: str):
+    ret = {'return': ''}
+    if number.isnumeric():
+        ret['return'] = sympy.isprime(int(number))
+    return ret
