e15ef31d3225e01218a6fd6ce9000a44edaabfce
Maciej <ms231717@student.polsl.pl>
2022-12-04 23:28:44 +0100

    Updated API. Images colors manipulation added.

--- src/app/main.py
+++ src/app/main.py
@@ -2,9 +2,12 @@ import http.client
 import json
 import os
 from datetime import datetime
+
+import cv2
 import sympy
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, UploadFile
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 MY_DOMAIN = os.environ.get('AUTH0_SRV', '')
@@ -54,3 +57,16 @@ async def get_time(token: HTTPAuthorizationCredentials = Depends(HTTPBearer())):
         raise HTTPException(status_code=401, detail="Token expired or invalid")
     else:
         return datetime.now().time()
+
+
+@app.post("/picture/invert")
+async def picture_inverting(file: UploadFile):
+    contents = await file.read()
+    if not os.path.exists("testfiles"):
+        os.makedirs("testfiles")
+    with open(f"testfiles//{file.filename}", "wb") as f:
+        f.write(contents)
+    img = cv2.imread(f"testfiles//{file.filename}")
+    img_invert = cv2.bitwise_not(img)
+    cv2.imwrite(f"testfiles//inv_{file.filename}", img_invert)
+    return FileResponse(f"testfiles//inv_{file.filename}")
